1d6fa982d86d5a87101631a35dea7128dd5d8a68
Sergio Marentes Jimenez <sergiomarentes4@gmail.com>
2022-12-10 21:29:20 -0600

    D:

--- ProyectoFinal/bdproyecto/usuarios.sql
+++ ProyectoFinal/bdproyecto/usuarios.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-11-2022 a las 04:33:35
+-- Tiempo de generación: 11-12-2022 a las 04:23:32
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -20,23 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `bdproyecto`
 --
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `intentos_usuarios`
---
-
-CREATE TABLE `intentos_usuarios` (
-  `password` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `intentos_usuarios`
---
-
-INSERT INTO `intentos_usuarios` (`password`) VALUES
-('0');
 
 -- --------------------------------------------------------
 
@@ -59,12 +42,7 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `usuario`, `email`, `token`, `codigo`, `fecha`, `contraseña`) VALUES
-(1, 'carlos', 'lozano.carlos.1am@gmail.com', '0', 0, 0, '89e6d2b383471fc370d828e552c19e65'),
-(2, 'zazuisa', 'c4rl1t0s.peipal@hotmail.com', '0', 0, 0, '8dd7a36bbc8d438d5f0f35a299fd5bd5'),
-(3, 'Sebasss', 'sebas@gmail.com', '0', 0, 0, '827ccb0eea8a706c4c34a16891f84e7b'),
-(7, 'Sebastián', 'smtz48957@gmail.com', '', 0, 0, '827ccb0eea8a706c4c34a16891f84e7b'),
-(14, '', 'smtz48957@gmail.com', 'f315d64d9a', 6714, 0, ''),
-(15, '', 'smtz48957@gmail.com', '2c83614ec1', 5161, 0, '');
+(25, 'admin', 'ropasilky@gmail.com', '', 0, 0, 'a89eea3a123b8d19db80f349fac804ff');
 
 --
 -- Índices para tablas volcadas
@@ -84,7 +62,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
