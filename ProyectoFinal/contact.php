8d01d0faa231d6daa2517e66f5dc7cf6b50fe027
Sebastián <smtz48957@gmail.comgit config --global user.name Sebastiangit config --global user.email smtz48957@gmail.comgit config --global user.email smtz48957@gmail.com>
2022-12-11 13:06:17 -0600

    contact.php

--- ProyectoFinal/contact.php
+++ ProyectoFinal/contact.php
@@ -1,5 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+</head>
+<body>	
+</body>
+</html>
+<script>
 <?php 
-
+function validar(){
 	session_start();
 
 	if ( isset($_POST['token']) ) {
@@ -8,8 +21,10 @@
 			header("Location: B V2/B V2/login.php");
 
 		} else {
-			echo "CAPTCHA incorrecto";
+			swal("CAPTCHA incorrecot", "Intentalo de nuevo", "error");
 		}
 		
 	}
+}
 ?>
+</script>
