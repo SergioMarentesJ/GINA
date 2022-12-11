f84dbd905f7e3c70cf6992da39cef214676dd6d0
Sergio Marentes Jimenez <sergiomarentes4@gmail.com>
2022-12-11 15:03:18 -0600

    Arreglar captcha

--- ProyectoFinal/contact.php
+++ ProyectoFinal/contact.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-</head>
-<body>	
-</body>
-</html>
-<script>
 <?php 
-function validar(){
+
 	session_start();
 
 	if ( isset($_POST['token']) ) {
@@ -21,10 +8,8 @@ function validar(){
 			header("Location: B V2/B V2/login.php");
 
 		} else {
-			swal("CAPTCHA incorrecot", "Intentalo de nuevo", "error");
+			echo "CAPTCHA incorrecto";
 		}
 		
 	}
-}
 ?>
-</script>
